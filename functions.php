ac9b4fc3641d259d9289bad71fc320bb39d8e64b
Emanuel Tannert <post@etannert.de>
2018-11-29 16:12:53 +0100

    add hook that runs after content is changed (still empty ...)

--- functions.php
+++ functions.php
@@ -17,6 +17,24 @@ $is_frontend_req = (!preg_match('/^(admin|login|wp-content)/', $current_path)
                     && (!is_admin() || (defined('DOING_AJAX') && DOING_AJAX)));
 
 
+
+
+// find out if advanced custom fields is active
+include_once(ABSPATH . 'wp-admin/includes/plugin.php');
+$is_acf_active = (is_plugin_active('advanced-custom-fields/acf.php')
+                  || is_plugin_active('advanced-custom-fields-pro/acf.php'));
+
+
+// register hook to run when content is changed
+$on_save_post = function() {
+    // TODO
+};
+add_action('save_post', $on_save_post, 20);
+if ($is_acf_active) {
+    add_action('acf/save_post', $on_save_post, 20);
+}
+
+
 // if this is an admin panel request, do nothing more
 if (!$is_frontend_req) return;
 
